9fa03c53008c4e7431f98eb95da0d791b84cf795
Simon  Date <simondate@yahoo.co.uk>
2016-10-05 16:12:52 +0100

    Updated documentation for public release

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# Adapt-google-analytics
+# adapt-google-analytics
 
-
-![alt tag](http://g.recordit.co/x2E4OcTSKp.gif)
-
-An extension that uses intro.js with Adapt to present an overlay to be displayed explaining how the elements of a course are used. Recommended for larger courses or large courses where learners will be spending lots of time in Adapt.
+An extension that enables you to easily implement Google Analytics into an Adapt course. Framework and AT compliant.
--- js/adapt-google-analytics.js
+++ js/adapt-google-analytics.js
@@ -3,12 +3,8 @@ define(function(require) {
     var Adapt = require('coreJS/adapt');
     var pageView = require('core/js/views/pageView');
 
-    // This should add/update progress on menuView
-
 	Adapt.on("pageView:ready", function() {
 		var gaIdentifier = Adapt.config.attributes._googleAnalytics._identifier;
-
-        // Create the element
         var GaTag = document.createElement("script");
         var GaScript =  "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', '" + gaIdentifier + "', 'auto', {'name': 'siteLinker','allowLinker': true});ga('require', 'linker');ga('require', 'displayfeatures');ga('send', 'pageview');ga('siteLinker.send', 'pageview');"
         GaTag.innerHTML = GaScript;
