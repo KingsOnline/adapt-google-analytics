5f817028392bd6da2fb6348c14d09644f69ccafd
Simon  Date <simondate@yahoo.co.uk>
2016-10-06 16:27:01 +0100

    Deleted unneeded code.

--- js/adapt-google-analytics.js
+++ js/adapt-google-analytics.js
@@ -13,22 +13,12 @@ define(function(require) {
     });
 
 	Adapt.on("pageView:ready", function() {
-		var gaIdentifier = Adapt.config.attributes._googleAnalytics._identifier;
-        var GaTag = document.createElement("script");
-        var GaScript =  "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', '" + gaIdentifier + "', 'auto', {'name': 'siteLinker','allowLinker': true});ga('require', 'linker');ga('require', 'displayfeatures');ga('send', 'pageview');ga('siteLinker.send', 'pageview');"
-        GaTag.innerHTML = GaScript;
-        document.head.appendChild(GaTag);
         ga('set', 'page', getUrl());
 		ga('send', 'pageview');
     });
 
 
 	Adapt.on("menuView:ready", function() {
-		var gaIdentifier = Adapt.config.attributes._googleAnalytics._identifier;
-        var GaTag = document.createElement("script");
-        var GaScript =  "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', '" + gaIdentifier + "', 'auto', {'name': 'siteLinker','allowLinker': true});ga('require', 'linker');ga('require', 'displayfeatures');ga('send', 'pageview');ga('siteLinker.send', 'pageview');"
-        GaTag.innerHTML = GaScript;
-        document.head.appendChild(GaTag);
         ga('set', 'page', getUrl());
 		ga('send', 'pageview');
     });
