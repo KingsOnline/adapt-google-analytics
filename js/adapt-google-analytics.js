6824de0de1494a48fd089a6a31e205b47d612f1b
Simon  Date <simondate@yahoo.co.uk>
2016-10-27 17:19:50 +0100

    listed

--- js/adapt-google-analytics.js
+++ js/adapt-google-analytics.js
@@ -2,22 +2,22 @@ define([
     'core/js/adapt'
 ], function(Adapt) {
 
-	Adapt.on("app:dataReady", function() {
-		var gaIdentifier = Adapt.config.attributes._googleAnalytics._identifier;
+    Adapt.on("app:dataReady", function() {
+        var gaIdentifier = Adapt.config.attributes._googleAnalytics._identifier;
         var GaTag = document.createElement("script");
-        var GaScript =  "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', '" + gaIdentifier + "', 'auto', {'name': 'siteLinker','allowLinker': true});ga('require', 'linker');ga('require', 'displayfeatures');ga('send', 'pageview');ga('siteLinker.send', 'pageview');"
+        var GaScript = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', '" + gaIdentifier + "', 'auto', {'name': 'siteLinker','allowLinker': true});ga('require', 'linker');ga('require', 'displayfeatures');ga('send', 'pageview');ga('siteLinker.send', 'pageview');"
         GaTag.innerHTML = GaScript;
         document.head.appendChild(GaTag);
     });
 
-	Adapt.on("pageView:ready", function() {
+    Adapt.on("pageView:ready", function() {
         ga('set', 'page', getUrl());
-		ga('send', 'pageview');
+        ga('send', 'pageview');
     });
 
-	Adapt.on("menuView:ready", function() {
+    Adapt.on("menuView:ready", function() {
         ga('set', 'page', getUrl());
-		ga('send', 'pageview');
+        ga('send', 'pageview');
     });
 
     function getUrl() {
