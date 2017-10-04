75069740ac0e5881d8a45c9c3c42472727730733
Simon  Date <simondate@yahoo.co.uk>
2017-10-04 12:21:39 +0100

    Check to see if attribute exists.

--- js/adapt-google-analytics.js
+++ js/adapt-google-analytics.js
@@ -1,21 +1,23 @@
-define([
-    'core/js/adapt'
-], function(Adapt) {
+define(['core/js/adapt'], function(Adapt) {
 
-    Adapt.on("app:dataReady", function() {
-        var gaIdentifier = Adapt.config.get('_googleAnalytics')._identifier;
-        var GaTag = document.createElement("script");
-        var GaScript = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', '" + gaIdentifier + "', 'auto', {'name': 'siteLinker','allowLinker': true});ga('require', 'linker');ga('require', 'displayfeatures');ga('send', 'pageview');ga('siteLinker.send', 'pageview');"
-        GaTag.innerHTML = GaScript;
-        document.head.appendChild(GaTag);
-    });
+  Adapt.on("app:dataReady", function() {
+    if (!Adapt.config.get('_googleAnalytics') || !Adapt.config.get('_googleAnalytics')._isEnabled)
+      return;
+    var gaIdentifier = Adapt.config.get('_googleAnalytics')._identifier;
+    var GaTag = document.createElement("script");
+    var GaScript = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', '" + gaIdentifier + "', 'auto', {'name': 'siteLinker','allowLinker': true});ga('require', 'linker');ga('require', 'displayfeatures');ga('send', 'pageview');ga('siteLinker.send', 'pageview');"
+    GaTag.innerHTML = GaScript;
+    document.head.appendChild(GaTag);
+  });
 
-    Adapt.on("pageView:ready menuView:ready", function() {
-        ga('set', 'page', getUrl());
-        ga('send', 'pageview');
-    });
+  Adapt.on("pageView:ready menuView:ready", function() {
+    if (!Adapt.config.get('_googleAnalytics') || !Adapt.config.get('_googleAnalytics')._isEnabled)
+      return;
+    ga('set', 'page', getUrl());
+    ga('send', 'pageview');
+  });
 
-    function getUrl() {
-        return location.pathname + location.hash;
-    }
+  function getUrl() {
+    return location.pathname + location.hash;
+  }
 });
