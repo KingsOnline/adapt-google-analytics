5a079d10e5ad1d8ce94bc34d1b4beab80316ea73
Simon  Date <simondate@yahoo.co.uk>
2017-10-04 13:37:40 +0100

    Can track the pressing of a button for print page feature

--- js/adapt-google-analytics.js
+++ js/adapt-google-analytics.js
@@ -5,7 +5,7 @@ define(['core/js/adapt'], function(Adapt) {
       return;
     var gaIdentifier = Adapt.config.get('_googleAnalytics')._identifier;
     var GaTag = document.createElement("script");
-    var GaScript = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', '" + gaIdentifier + "', 'auto', {'name': 'siteLinker','allowLinker': true});ga('require', 'linker');ga('require', 'displayfeatures');ga('send', 'pageview');ga('siteLinker.send', 'pageview');"
+    var GaScript = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', '" + gaIdentifier + "', 'auto');"
     GaTag.innerHTML = GaScript;
     document.head.appendChild(GaTag);
   });
@@ -15,7 +15,20 @@ define(['core/js/adapt'], function(Adapt) {
       return;
     ga('set', 'page', getUrl());
     ga('send', 'pageview');
+    console.log('working');
+    printPageListener();
   });
+
+  function printPageListener() {
+    if(Adapt.config.get('_googleAnalytics')._interactions._printPage) {
+
+      $('body').on('click', '.printPage-icon', function() {
+          console.log('print page fired');
+          ga('send', 'event', 'Button', 'Click', 'Print page');
+      });
+
+    }
+  }
 
   function getUrl() {
     return location.pathname + location.hash;
